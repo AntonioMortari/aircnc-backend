095770e1fee02486b862239ac6bc835d9bbc5f25
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2024-02-24 11:51:16 -0300

    feat: added cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
 import { errorMiddleware } from './middlewares/error';
 import { usersRoutes } from './routes/users.routes';
 import { spotsRoutes } from './routes/spots.routes';
@@ -11,7 +13,9 @@ import { errors } from 'celebrate';
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(express.urlencoded({ extended: true }));
+server.use('/files', express.static(path.resolve(__dirname, '..', '..', 'public', 'uploads')));
 
 // routes
 server.use('/users', usersRoutes);
